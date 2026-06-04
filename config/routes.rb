6522918e0dc262ca9d6ec64504c1ee8a5f3c6727
Levi <l.breakwell@me.com>
2026-06-04 12:54:38 +1000

    Fix route naming conflict between singleton membership and memberships collection

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :teams, only: %i[new create show] do
     member { patch :regenerate_invite }
     resource :spawtz_setup, only: %i[new create]
-    resource :membership, only: %i[create destroy]
+    resource :membership, only: %i[create destroy], as: :join
     resources :memberships, only: %i[index update], controller: "team_memberships"
     resources :players, only: %i[index new create destroy] do
         member { patch :claim }
